ed85f781021d92bfe715ea2adb7ba6e7d349f9e0
lehoangnnx <lehoangnnx@gmail.com>
2017-11-10 10:45:22 +0700

    Fix Forgot Password

--- source/DuAn2/src/main/java/com/javaweb/controller/ForgotPassword.java
+++ source/DuAn2/src/main/java/com/javaweb/controller/ForgotPassword.java
@@ -1,12 +1,14 @@
 package com.javaweb.controller;
 
+import com.javaweb.model.Users;
 import com.javaweb.service.UsersService;
-import org.apache.catalina.User;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ForgotPassword {
@@ -14,9 +16,9 @@ public class ForgotPassword {
     UsersService  usersService;
     @GetMapping("/forgotpassword/{key}")
     public String forgotPassword(@PathVariable("key") String key){
-        User user = null;
+        Users user = null;
         try {
-            users = usersService.findByForgotPassword(key);
+            user = usersService.findByForgotPassword(key);
             if (user == null){
                 return "redirect:/403";
             }
@@ -27,5 +29,9 @@ public class ForgotPassword {
         return "forgotpassword";
     }
 
-    @PostMapping
+    @PostMapping("/forgotpassword")
+    public String forgotPassword(){
+
+        return "/";
+    }
 }
