a841053919311829e49e5c040cdd26716d186138
lehoangnnx <lehoangnnx@gmail.com>
2017-11-14 08:42:08 +0700

    Delete Project

--- source/DuAn2/src/main/java/com/javaweb/controller/administrator/AdminTagsController.java
+++
@@ -1,6 +0,0 @@
-package com.javaweb.controller.administrator;
-
-
-public class AdminTagsController {
-
-}
--- source/DuAn2/src/main/java/com/javaweb/service/PagesService.java
+++
@@ -1,9 +0,0 @@
-package com.javaweb.service;
-
-import java.util.List;
-
-import com.javaweb.model.Pages;
-
-public interface PagesService {
-	List<Pages> findAll();
-}
